32ad8fefd756685696c5de3087ca84a7c944a557
Mircea Popa <pmircea027@gmail.com>
2022-12-29 02:01:09 +0200

    fixel mobile navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,6 +25,15 @@ const NavbarInstance = () => {
 
                     </Navbar.Toggle>
                     <h1 className="header-mobile d-lg-none d-xl-none">Noblesse</h1>
+                    <Navbar.Brand href="/en" style={{ color: "#fff" }} className="d-lg-none d-xl-none">
+                        <svg
+                            id="logo" width="30" height="29" viewBox="0 0 30 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15.3486 27.86C22.7658 27.86 28.7786 21.8472 28.7786 14.43C28.7786 7.01281 22.7658 1 15.3486 1C7.93139 1 1.91858 7.01281 1.91858 14.43C1.91858 21.8472 7.93139 27.86 15.3486 27.86Z" stroke="#CAEC7D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M1.91858 14.43H28.7786" stroke="#CAEC7D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M15.3486 1C18.7078 4.67761 20.6168 9.4502 20.7206 14.43C20.6168 19.4098 18.7078 24.1824 15.3486 27.86C11.9893 24.1824 10.0803 19.4098 9.97656 14.43C10.0803 9.4502 11.9893 4.67761 15.3486 1V1Z" stroke="#CAEC7D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                        RO
+                    </Navbar.Brand >
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
                             className="me-auto my-2 my-lg-0"
@@ -47,7 +56,7 @@ const NavbarInstance = () => {
                             </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
-                    <Navbar.Brand href="/en" style={{ color: "#fff" }}>
+                    <Navbar.Brand href="/en" style={{ color: "#fff" }} className="d-none d-lg-flex">
                         <svg
                             id="logo" width="30" height="29" viewBox="0 0 30 29" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M15.3486 27.86C22.7658 27.86 28.7786 21.8472 28.7786 14.43C28.7786 7.01281 22.7658 1 15.3486 1C7.93139 1 1.91858 7.01281 1.91858 14.43C1.91858 21.8472 7.93139 27.86 15.3486 27.86Z" stroke="#CAEC7D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -56,6 +65,7 @@ const NavbarInstance = () => {
                         </svg>
                         RO
                     </Navbar.Brand >
+                    
                 </Container>
             </Navbar>
         );
