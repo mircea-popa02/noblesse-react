f9913c7ab102b6764d9d93eaafd5c2845c9f0d37
Mircea Popa <pmircea027@gmail.com>
2024-02-16 13:49:45 +0200

    Carousel overhaul + english support

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,94 +1,62 @@
-import Carousel from 'react-bootstrap/Carousel';
-import React from 'react';
-import './Carousel.css'
-import { storage } from '../firebase';
-import { ref } from "firebase/storage";
-import { useEffect, useState } from 'react';
-import { listAll, getDownloadURL } from 'firebase/storage';
-import { getMetadata } from "firebase/storage";
+import Carousel from "react-bootstrap/Carousel";
+import React from "react";
+import "./Carousel.css";
+import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from '../firebase';
+import { db } from "../firebase";
 
-function CarouselFadeExample() {
-    const [images, setImages] = useState([]);
-    const storageRef = ref(storage, "images/");
+function CarouselFadeExample(props) {
+  const [itemInfo, setItemInfo] = useState([]);
+  const language = props.language;
 
-    useEffect(() => {
-        listAll(storageRef).then((res) => {
-            res.items.forEach((storageRef) => {
-                getDownloadURL(storageRef).then((url) => {
-                    setImages((images) => [...images, url]);
-                });
-            });
-        })
-    }, []);
+  const fetchPost = async () => {
+    await getDocs(collection(db, "slideshow"))
+      .then((querySnapshot) => {
+        const newData = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setItemInfo(newData[0].metadata);
+        console.log(newData[0].metadata);
+      })
+      .catch((error) => {
+        console.error("Error fetching slideshow metadata:", error);
+      });
+  };
 
+  useEffect(() => {
+    fetchPost();
+  }, []);
 
-    for (let i = 0; i < images.length; i++) {
-        let imageRef = ref(storage, images[i]);
-        getMetadata(imageRef).then((metadata) => {
-        }
-        )
+  return (
+    <Carousel fade>
+      {itemInfo.map((item, index) => {
+        return (
+          <Carousel.Item key={index}>
+            {item.type === "photo" ? (
+              <img className="d-block w-100" src={item.link} alt={item.alt} />
+            ) : (
+              <video
+                className="d-block w-100"
+                src={item.link}
+                alt={item.alt}
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                autoPlay
+                loop
+              />
+            )}
 
-    }
-
-    const [todos, setTodos] = useState([]);
-
-    const fetchPost = async () => {
-
-        await getDocs(collection(db, "descriptions"))
-            .then((querySnapshot) => {
-                const newData = querySnapshot.docs
-                    .map((doc) => ({ ...doc.data(), id: doc.id }));
-                setTodos(newData);
-            })
-    }
-
-    useEffect(() => {
-        fetchPost();
-    }, [])
-
-
-    return (
-        <Carousel fade>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={images[0]}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>Aranajmente florare pentru </h3>
-                    <h3> orice ocazie</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={images[1]}
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-                    <h3>Nu știi ce să îi cumperi?</h3>
-                    <p>Un buchet de flori va impresiona întotdeauna</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={images[2]}
-                    alt="Third slide"
-                />
-                <Carousel.Caption>
-                    <h3>Pregătim cadoul perfect</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
-    );
+            <Carousel.Caption>
+              <div className="carousel-text">
+                <h1>{language === "RO" ? item.title.ro : item.title.en}</h1>
+                <p>{language === "RO" ? item.subtitle.ro : item.subtitle.en}</p>
+              </div> 
+            </Carousel.Caption>
+          </Carousel.Item>
+        );
+      })}
+    </Carousel>
+  );
 }
 
 export default CarouselFadeExample;
