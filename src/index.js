2644306f8806eea25ab544e604d31280d0f13ef2
Mircea Popa <pmircea027@gmail.com>
2025-03-01 20:33:40 +0200

    chore: update .gitignore, wrangler.toml, and package.json; refactor Router to BrowserRouter in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import App from "./App";
 import Gallery from "./components/Gallery";
 import NotFound from "./components/NotFound";
@@ -10,11 +10,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Router>
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/gallery" element={<Gallery />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
-  </Router>
+  </BrowserRouter>
 );
